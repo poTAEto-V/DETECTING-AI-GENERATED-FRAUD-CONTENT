3577c7114284875c6d6592284e33cc15039b9e61
poTAEto-V <kimlavya2013.7v@gmail.com>
2026-02-17 00:49:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
-import random
+from flask import Flask, render_template, request
+import os
+
+# IMPORT YOUR AI FILE
+from fraud_detector import analyze_message
 
 app = Flask(__name__)
+
 
 # HOME PAGE
 @app.route("/")
@@ -9,30 +13,29 @@ def home():
     return render_template("index.html")
 
 
-# ANALYZE ROUTE (FORM SUBMIT)
+# ANALYZE ROUTE
 @app.route("/analyze", methods=["POST"])
 def analyze():
 
     message = request.form.get("message")
 
-    # fake AI score (later model laga dena)
-    score = random.randint(10, 100)
-
-    if score >= 70:
-        level = "CRITICAL FRAUD"
-    elif score >= 40:
-        level = "SUSPICIOUS"
-    else:
-        level = "SAFE"
+    # CALL YOUR AI FUNCTION
+    score, level, reasons, words, ai_prob, creds, urls = analyze_message(message)
 
     return render_template(
         "result.html",
-        text=message,
+        message=message,
         score=score,
-        level=level
+        level=level,
+        reasons=reasons,
+        words=words,
+        ai_prob=ai_prob,
+        creds=creds,
+        urls=urls
     )
 
 
+# RENDER PORT
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
