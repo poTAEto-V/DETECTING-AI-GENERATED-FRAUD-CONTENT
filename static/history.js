dae858858adbf5cbb6eb5383bb74465c87883d3f
poTAEto-V <kimlavya2013.7v@gmail.com>
2026-02-17 00:17:56 +0530

    Create history.js

---
+++ static/history.js
@@ -0,0 +1,92 @@
+[
+  {
+    "text": "share your otp please",
+    "score": 30,
+    "level": "SAFE"
+  },
+  {
+    "text": "its very urgent , because your bank account is hacked quickly",
+    "score": 35,
+    "level": "SUSPICIOUS"
+  },
+  {
+    "text": "10000 rupees is deopsited to your account",
+    "score": 0,
+    "level": "SAFE"
+  },
+  {
+    "text": "you win a super car, give 4 digit otp",
+    "score": 30,
+    "level": "SAFE"
+  },
+  {
+    "text": "high alert , give me otp its very urgent\r\n",
+    "score": 45,
+    "level": "SUSPICIOUS"
+  },
+  {
+    "text": "Hello please share your OTP unless your bank account will be freeze.",
+    "score": 50,
+    "level": "SUSPICIOUS"
+  },
+  {
+    "text": "Hello please share your OTP unless your bank account will be freeze.",
+    "score": 50,
+    "level": "SUSPICIOUS"
+  },
+  {
+    "text": "Hello.",
+    "score": 0,
+    "level": "SAFE"
+  },
+  {
+    "text": "Hello, I'm speaking from SBI bank.",
+    "score": 20,
+    "level": "SAFE"
+  },
+  {
+    "text": "Its urgent, send otp",
+    "score": 45,
+    "level": "SUSPICIOUS"
+  },
+  {
+    "text": "Share otp",
+    "score": 30,
+    "level": "SAFE"
+  },
+  {
+    "text": "Share your otp",
+    "score": 30,
+    "level": "SAFE"
+  },
+  {
+    "text": "Yoyy",
+    "score": 0,
+    "level": "SAFE"
+  },
+  {
+    "text": "urgent: your bank account is blocked.\r\nverify now: http://bank-secure-login.com\r\nenter your OTP to restore access",
+    "score": 100,
+    "level": "CRITICAL FRAUD"
+  },
+  {
+    "text": "please share your OTP",
+    "score": 30,
+    "level": "SAFE"
+  },
+  {
+    "text": "Your bank has been held, please share ODP.",
+    "score": 20,
+    "level": "SAFE"
+  },
+  {
+    "text": "Held Health.",
+    "score": 0,
+    "level": "SAFE"
+  },
+  {
+    "text": "Your bank has been hacked. Please share your OTP.",
+    "score": 50,
+    "level": "SUSPICIOUS"
+  }
+]
