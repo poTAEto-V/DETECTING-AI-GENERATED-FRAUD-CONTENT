906ae4669f7d8b2466f12ffc095fc55f927f7341
poTAEto-V <kimlavya2013.7v@gmail.com>
2026-02-17 00:28:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
 
+# Home Page
 @app.route("/")
 def home():
-    # fake risk value (later tum model se replace karna)
+    return render_template("index.html")
+
+
+# Analyze Page (FORM yaha aayega)
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    message = request.form["message"]
+
+    # abhi fake result (later AI model)
     risk = random.randint(10, 95)
 
-    return render_template("index.html", risk=risk)
+    if risk > 60:
+        result = "⚠️ Fraud Message Detected"
+    else:
+        result = "✅ Safe Message"
+
+    return render_template("result.html", result=result, message=message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
