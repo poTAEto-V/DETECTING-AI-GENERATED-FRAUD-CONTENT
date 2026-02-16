1334b26e6596b007673b2032efd69409148d642f
poTAEto-V <kimlavya2013.7v@gmail.com>
2026-02-17 00:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask, render_template, request
 import os
 
-# IMPORT YOUR AI FILE
-from fraud_detector import analyze_message
+try:
+    from fraud_detector import analyze_message
+except Exception as e:
+    print("IMPORT ERROR:", e)
 
 app = Flask(__name__)
 
 
-# HOME PAGE
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# ANALYZE ROUTE
 @app.route("/analyze", methods=["POST"])
 def analyze():
-
     message = request.form.get("message")
 
-    # CALL YOUR AI FUNCTION
     score, level, reasons, words, ai_prob, creds, urls = analyze_message(message)
 
     return render_template(
@@ -35,7 +33,7 @@ def analyze():
     )
 
 
-# RENDER PORT
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
