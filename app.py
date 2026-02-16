a9244f6ff9ce21eadff1c4dfdf59594e7bcbe4a6
poTAEto-V <kimlavya2013.7v@gmail.com>
2026-02-17 00:58:36 +0530

    Refactor analyze_message import and add input validation
    Removed import error handling for analyze_message and added safety check for message input.

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template, request
+frfrom flask import Flask, render_template, request
 import os
-
-try:
-    from fraud_detector import analyze_message
-except Exception as e:
-    print("IMPORT ERROR:", e)
+from fraud_detector import analyze_message
 
 app = Flask(__name__)
 
 
+# HOME PAGE
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+# ANALYZE MESSAGE
 @app.route("/analyze", methods=["POST"])
 def analyze():
+
     message = request.form.get("message")
+
+    # safety check
+    if not message:
+        return "No message received"
 
     score, level, reasons, words, ai_prob, creds, urls = analyze_message(message)
 
@@ -33,7 +36,9 @@ def analyze():
     )
 
 
+# RENDER PORT
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
